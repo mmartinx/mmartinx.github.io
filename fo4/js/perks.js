1bf9f850f3e0b1f18c6e8005d97733886f681d96
Marshall Martin <mmartinx@gmail.com>
2015-11-13 13:54:08 -0500

    Initi load

---
+++ fo4/js/perks.js
@@ -0,0 +1,479 @@
+﻿var perks = [
+    {
+        special: 'st',
+        perks: [
+            {
+                rank: 1,
+                name: 'Iron Fist',
+                img: 'iron-fist.png',
+                ranks: 5,
+                ranked: [
+                    {
+                        rank: 1,
+                        description: 'Channel your chi to unleash devastating fury! Punching attacks now do 20% more damage.'
+                    },
+                    {
+                        rank: 2,
+                        description: 'Punching attacks now do 40% more damage and can disarm your opponent.'
+                    },
+                    {
+                        rank: 3,
+                        description: 'Punching attacks now do 60% more damage. Unarmed Power Attacks have a chance to cripple one of your opponent\'s limbs.'
+                    },
+                    {
+                        rank: 4,
+                        description: 'Attacks made with fists or weapons worn on the hands 80% more damage. Unarmed Power Attacks have an increased chance to cripple one of your opponent\'s limbs.'
+                    },
+                    {
+                        rank: 5,
+                        description: 'Attacks made with fists or weapons worn on the hands inflict 100% more damage. Criticals in V.A.T.S. will paralyse your opponents.'
+                    }
+                ]
+            },
+            {
+                rank: 2,
+                name: 'Big Leagues',
+                img: 'big-leagues.png',
+                ranks: 5
+            },
+            {
+                rank: 3,
+                name: 'Armorer',
+                img: 'armorer.png',
+                ranks: 4
+            },
+            {
+                rank: 4,
+                name: 'Blacksmith',
+                img: 'blacksmith.png',
+                ranks: 3
+            },
+            {
+                rank: 5,
+                name: 'Heavy Gunner',
+                img: 'heavy-gunner.png',
+                ranks: 5
+            },
+            {
+                rank: 6,
+                name: 'Strong Back',
+                img: 'strong-back.png',
+                ranks: 4
+            },
+            {
+                rank: 7,
+                name: 'Steady Aim',
+                img: 'steady-aim.png',
+                ranks: 2
+            },
+            {
+                rank: 8,
+                name: 'Basher',
+                img: 'basher.png',
+                ranks: 4
+            },
+            {
+                rank: 9,
+                name: 'Rooted',
+                img: 'rooted.png',
+                ranks: 3
+            },
+            {
+                rank: 10,
+                name: 'Pain Train',
+                img: 'pain-train.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'pe',
+        perks: [
+            {
+                rank: 1,
+                name: 'Pickpocket',
+                img: 'pickpocket.png',
+                ranks: 4
+            },
+            {
+                rank: 2,
+                name: 'Rifleman',
+                img: 'rifleman.png',
+                ranks: 5
+            },
+            {
+                rank: 3,
+                name: 'Awareness',
+                img: 'awareness.png',
+                ranks: 1
+            },
+            {
+                rank: 4,
+                name: 'Locksmith',
+                img: 'locksmith.png',
+                ranks: 4
+            },
+            {
+                rank: 5,
+                name: 'Demolition Expert',
+                img: 'demolition-expert.png',
+                ranks: 4
+            },
+            {
+                rank: 6,
+                name: 'Night Person',
+                img: 'night-person.png',
+                ranks: 2
+            },
+            {
+                rank: 7,
+                name: 'Refractor',
+                img: 'refractor.png',
+                ranks: 5
+            },
+            {
+                rank: 8,
+                name: 'Sniper',
+                img: 'sniper.png',
+                ranks: 3
+            },
+            {
+                rank: 9,
+                name: 'Penetrator',
+                img: 'penetrator.png',
+                ranks: 2
+            },
+            {
+                rank: 10,
+                name: 'Concentrated Fire',
+                img: 'concentrated-fire.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'en',
+        perks: [
+            {
+                rank: 1,
+                name: 'Toughness',
+                img: 'toughness.png',
+                ranks: 5
+            },
+            {
+                rank: 2,
+                name: 'Lead Belly',
+                img: 'lead-belly.png',
+                ranks: 3
+            },
+            {
+                rank: 3,
+                name: 'Life Giver',
+                img: 'life-giver.png',
+                ranks: 3
+            },
+            {
+                rank: 4,
+                name: 'Chem Resistant',
+                img: 'chem-resistant.png',
+                ranks: 2
+            },
+            {
+                rank: 5,
+                name: 'Aquaboy',
+                img: 'aquaboy.png',
+                ranks: 2
+            },
+            {
+                rank: 6,
+                name: 'Rad Resistant',
+                img: 'rad-resistant.png',
+                ranks: 3
+            },
+            {
+                rank: 7,
+                name: 'Adamantium Skeleton',
+                img: 'adamantium-skeleton.png',
+                ranks: 3
+            },
+            {
+                rank: 8,
+                name: 'Cannibal',
+                img: 'cannibal.png',
+                ranks: 3
+            },
+            {
+                rank: 9,
+                name: 'Ghoulish',
+                img: 'ghoulish.png',
+                ranks: 3
+            },
+            {
+                rank: 10,
+                name: 'Solar Powered',
+                img: 'solar-powered.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'ch',
+        perks: [
+            {
+                rank: 1,
+                name: 'Cap Collector',
+                img: 'cap-collector.png',
+                ranks: 3
+            },
+            {
+                rank: 2,
+                name: 'Lady Killer',
+                img: 'lady-killer-man-hunter.png',
+                ranks: 3
+            },
+            {
+                rank: 3,
+                name: 'Lone Wanderer',
+                img: 'lone-wanderer.png',
+                ranks: 3
+            },
+            {
+                rank: 4,
+                name: 'Attack Dog',
+                img: 'attack-dog.png',
+                ranks: 3
+            },
+            {
+                rank: 5,
+                name: 'Animal Friend',
+                img: 'animal-friend.png',
+                ranks: 3
+            },
+            {
+                rank: 6,
+                name: 'Local Leader',
+                img: 'local-leader.png',
+                ranks: 2
+            },
+            {
+                rank: 7,
+                name: 'Party Boy',
+                img: 'party-boy-party-girl.png',
+                ranks: 3
+            },
+            {
+                rank: 8,
+                name: 'Inspiration',
+                img: 'inspiration.png',
+                ranks: 3
+            },
+            {
+                rank: 9,
+                name: 'Wasteland Whisperer',
+                img: 'wasteland-whisperer.png',
+                ranks: 3
+            },
+            {
+                rank: 10,
+                name: 'Intimidation',
+                img: 'Intimidation.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'in',
+        perks: [
+            {
+                rank: 1,
+                name: 'V.A.N.S.',
+                img: 'vans.png',
+                ranks: 1
+            },
+            {
+                rank: 2,
+                name: 'Medic',
+                img: 'medic.png',
+                ranks: 4
+            },
+            {
+                rank: 3,
+                name: 'Gun Nut',
+                img: 'gun-nut.png',
+                ranks: 4
+            },
+            {
+                rank: 4,
+                name: 'Hacker',
+                img: 'hacker.png',
+                ranks: 4
+            },
+            {
+                rank: 5,
+                name: 'Scrapper',
+                img: 'scrapper.png',
+                ranks: 2
+            },
+            {
+                rank: 6,
+                name: 'Science',
+                img: 'science.png',
+                ranks: 4
+            },
+            {
+                rank: 7,
+                name: 'Chemist',
+                img: 'chemist.png',
+                ranks: 4
+            },
+            {
+                rank: 8,
+                name: 'Robotics Expert',
+                img: 'robotics-expert.png',
+                ranks: 3
+            },
+            {
+                rank: 9,
+                name: 'Nuclear Physicist',
+                img: 'nuclear-physicist.png',
+                ranks: 3
+            },
+            {
+                rank: 10,
+                name: 'Nerd Rage',
+                img: 'nerd-rage.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'ag',
+        perks: [
+            {
+                rank: 1,
+                name: 'Gunslinger',
+                img: 'gunslinger.png',
+                ranks: 5
+            },
+            {
+                rank: 2,
+                name: 'Commando',
+                img: 'commando.png',
+                ranks: 5
+            },
+            {
+                rank: 3,
+                name: 'Sneak',
+                img: 'sneak.png',
+                ranks: 5
+            },
+            {
+                rank: 4,
+                name: 'Mister Sandman',
+                img: 'mister-sandman.png',
+                ranks: 3
+            },
+            {
+                rank: 5,
+                name: 'Action Boy',
+                img: 'action-boy-action-girl.png',
+                ranks: 2
+            },
+            {
+                rank: 6,
+                name: 'Moving Target',
+                img: 'moving-target.png',
+                ranks: 3
+            },
+            {
+                rank: 7,
+                name: 'Ninja',
+                img: 'ninja.png',
+                ranks: 3
+            },
+            {
+                rank: 8,
+                name: 'Quick Hands',
+                img: 'quick-hands.png',
+                ranks: 2
+            },
+            {
+                rank: 9,
+                name: 'Blitz',
+                img: 'blitz.png',
+                ranks: 2
+            },
+            {
+                rank: 10,
+                name: 'Gun Fu',
+                img: 'gun-fu.png',
+                ranks: 3
+            }
+        ]
+    },
+    {
+        special: 'lu',
+        perks: [
+            {
+                rank: 1,
+                name: 'Fortune Finder',
+                img: 'fortune-finder.png',
+                ranks: 4
+            },
+            {
+                rank: 2,
+                name: 'Scrounger',
+                img: 'scrounger.png',
+                ranks: 4
+            },
+            {
+                rank: 3,
+                name: 'Bloody Mess',
+                img: 'bloody-mess.png',
+                ranks: 4
+            },
+            {
+                rank: 4,
+                name: 'Mysterious Stranger',
+                img: 'mysterious-stranger.png',
+                ranks: 3
+            },
+            {
+                rank: 5,
+                name: 'Idiot Savant',
+                img: 'idiot-savant.png',
+                ranks: 3
+            },
+            {
+                rank: 6,
+                name: 'Better Criticals',
+                img: 'better-criticals.png',
+                ranks: 3
+            },
+            {
+                rank: 7,
+                name: 'Critical Banker',
+                img: 'critical-banker.png',
+                ranks: 3
+            },
+            {
+                rank: 8,
+                name: 'Grim Reaper\'s Sprint',
+                img: 'grim-reapers-sprint.png',
+                ranks: 3
+            },
+            {
+                rank: 9,
+                name: 'Four Leaf Clover',
+                img: 'four-leaf-clover.png',
+                ranks: 4
+            },
+            {
+                rank: 10,
+                name: 'Ricochet',
+                img: 'ricochet.png',
+                ranks: 3
+            }
+        ]
+    }
+];
---
+++ fo4/js/scripts.js
@@ -0,0 +1,180 @@
+﻿var totalPoints = 28;
+
+var renderPerks = function () {
+    var html = '',
+        special = getSPECIAL();
+
+    html += '<tr>';
+    for (var i = 0; i < special.length; ++i) {
+        html += '<th>' + special[i].special.toUpperCase() + ': ' + special[i].value + '</th>';
+    }
+    html += '</tr>';
+
+    for (var i = 0; i <= 9; ++i) {
+        html += '<tr>';
+        for (var j = 0; j < perks.length; ++j) {
+            var perk = perks[j].perks[i],
+                className = i > special[j].value - 1 ? ' unavailable' : '';
+
+            if (!perk.currentRank) {
+                perk.currentRank = 0;
+            }
+
+            var title = perk.name;
+            if (perk.ranked) {
+                title += '\r\n\r\n';
+                title += perk.ranked.map(function (rank) {
+                    return 'Rank ' + rank.rank + ': ' + rank.description;
+                }).join('\r\n');
+            }
+
+            html += '<td><div data-i="' + i + '" data-j="' + j + '" title="' + title + '" class="perk' + className + '" style="background-image:url(\'img/' + perk.img + '\');">';
+            if (className !== ' unavailable') {
+                html += '<div class="overlay"><button class="btn btn-xs btn-danger btn-dec-perk"><i class="glyphicon glyphicon-minus"></i></button>&nbsp;' + perk.currentRank + '/' + perk.ranks + '&nbsp;<button class="btn btn-xs btn-success btn-inc-perk"><i class="glyphicon glyphicon-plus"></i></button></div>';
+            }
+            html += '</td>';
+        }
+        html += '</tr>';
+    }
+    $('.table').html(html);
+}
+
+var getSPECIAL = function () {
+    return $('[data-special]').map(function () {
+        return {
+            special: $(this).data('special'),
+            value: $(this).find('input').val()
+        };
+    });
+};
+
+var requiredLevel = function () {
+    var total = 0;
+    for (var i = 0; i < perks.length; ++i) {
+        for (var j = 0; j < perks[i].perks.length; ++j) {
+            total += perks[i].perks[j].currentRank;
+        }
+    }
+
+    return total;
+}
+
+var renderRequiredLevel = function () {
+    $('.required-level').text(requiredLevel());
+}
+
+var renderAll = function () {
+    renderPerks();
+    calculatePoints();
+    renderRequiredLevel();
+    renderSummary();
+}
+
+var calculatePoints = function () {
+    var remaining = totalPoints - getAllocatedPoints();
+    if (includeBobbleheads()) {
+        remaining += 7;
+    }
+    $pointsLeft.text(remaining);
+}
+
+var getAllocatedPoints = function () {
+    return $('[data-special] input').map(function () {
+        return parseInt($(this).val());
+    }).get().reduce(function (prev, curr) {
+        return prev + curr;
+    });
+}
+
+var $pointsLeft = $('.points-left'),
+        $includeBobbleheads = $('.include-bobbleheads');
+
+var includeBobbleheads = function () {
+    return $includeBobbleheads.is(':checked');
+}
+
+var pointsRemaining = function () {
+    return parseInt($pointsLeft.text());
+}
+
+var renderSummary = function () {
+    var html = '';
+    for (var i = 0; i < perks.length; ++i) {
+        for (var j = 0; j < perks[i].perks.length; ++j) {
+            var perk = perks[i].perks[j];
+            if (perk.currentRank && perk.currentRank > 0) {
+                html += '<li>' + perk.name + ': ' + perk.currentRank + '/' + perk.ranks + '</li>';
+            }
+        }
+    }
+
+    $('.summary').html(html);
+}
+
+$(function () {
+    renderAll();
+
+    $includeBobbleheads.on('click', function () {
+        renderAll();
+    });
+
+    $('.btn-inc').on('click', function () {
+        var $li = $(this).parent().parent(),
+            $input = $li.find('input'),
+            value = parseInt($input.val()),
+            remaining = pointsRemaining();
+
+        if (remaining === 0)
+            return;
+
+        if (value < 10) {
+            $input.val(value + 1);
+        }
+
+        renderAll();
+    });
+
+    $('.btn-dec').on('click', function () {
+        var $li = $(this).parent().parent(),
+            $input = $li.find('input'),
+            value = parseInt($input.val()),
+            special = $li.data('special');
+
+        if (value > 1) {
+            $input.val(value - 1);
+
+            for (var i = 0; i < perks.length; ++i) {
+                if (perks[i].special === special) {
+                    for (var j = value - 1; j < perks[i].perks.length; ++j) {
+                        perks[i].perks[j].currentRank = 0;
+                    }
+                }
+            }
+        }
+
+        renderAll();
+    });
+
+    $('body').on('click', '.btn-inc-perk, .btn-dec-perk', function () {
+        var $container = $(this).parent().parent(),
+            i = parseInt($container.data('i')),
+            j = parseInt($container.data('j')),
+            perk = perks[j].perks[i],
+            incrementing = $(this).hasClass('btn-inc-perk');
+
+        if (!perk.currentRank)
+            perk.currentRank = 0;
+
+        if (incrementing) {
+            if (perk.currentRank < perk.ranks) {
+                perk.currentRank += 1;
+            }
+        } else {
+            if (perk.currentRank > 0) {
+                perk.currentRank -= 1;
+            }
+        }
+
+        renderAll();
+    });
+});
